918f830dbba062b26b3e2c2f0cc81f332d0ea281
Andri Faqih <realizy@student.telkomuniversity.ac.id>
2024-10-18 14:37:33 +0700

    first init.

---
+++ src/lib/components/Navbar/Navbar.svelte
@@ -0,0 +1,15 @@
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <a class="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+            <li><a>Home</a></li>
+            <li><a>Skills</a></li>
+            <li><a>Projects</a></li>
+            <li><a>Experience</a></li>
+            <li><a>Education</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    </div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    import "../app.css";
+    import Navbar from "$lib/components/Navbar/Navbar.svelte";
+</script>
+
+<svelte:head>
+    <title>Hello There~</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap');
+    </style>
+</svelte:head>
+
+<div class="container mx-auto">
+    <Navbar />
+    <div class="content container">
+        <slot />
+    </div>
+</div>
+
+<style lang="scss">
+
+
+</style>
