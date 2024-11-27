91fff31b206b3fa65d61e550dde7e22d12b36729
Andri Faqih <realizy@student.telkomuniversity.ac.id>
2024-11-27 17:24:14 +0700

    refix

--- svelte.config.js
+++ svelte.config.js
@@ -11,11 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown
-			out: 'build',
-			precompress: false,
-		}),
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
