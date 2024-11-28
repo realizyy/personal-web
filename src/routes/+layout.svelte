0bee1a29b5a2db198c910e0c23c4a40b3d80ebbb
Andri Faqih <realizy@student.telkomuniversity.ac.id>
2024-11-28 12:48:45 +0700

    remove font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,6 @@
 
 <svelte:head>
     <title>Hello There~</title>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap');
-    </style>
 </svelte:head>
 
 <div class="container content max-h-screen mx-auto">
